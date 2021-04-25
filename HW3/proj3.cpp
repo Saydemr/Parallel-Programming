52bc64fb9661929b7fb1f972e2f9f9213dd41d82
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2021-04-25 18:12:58 +0300

    Printing to csv

--- HW3/proj3.cpp
+++ HW3/proj3.cpp
@@ -1,9 +1,12 @@
-#include <stdio.h>
-#include <math.h>
 #include <omp.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
 
 unsigned int seed = 0;
+FILE* myfile;
 
 int NowYear;     // 2021 - 2026
 int NowMonth;       // 0 - 11
@@ -14,7 +17,7 @@ float   NowHeight;       // grain height in inches
 int NowNumDeer;         // number of deer in the current population
 int NowNumMushroom;     // number of mushrooms in the current population
 
-const float MAGIC_GROWS_PER_MONTH = 5.0;
+const int MAGIC_GROWS_PER_MONTH = 8;
 const float MIN_TEMP_MUSHROOM = 50.0;
 const float MAX_TEMP_MUSHROOM = 70.0;
 const float MIN_PRECIP_MUSHROOM = 9.0;
@@ -50,6 +53,10 @@ int Ranf( unsigned int *seedp, int ilow, int ihigh ) {
 
     return (int)(  Ranf(seedp, low,high) );}
 
+float min(float x, float y) 
+{
+	return (x < y ? x : y);
+}
 
 int main () {
     #ifndef _OPENMP
@@ -57,7 +64,10 @@ int main () {
     return 1;
     #endif
 
+    myfile = fopen("out.csv","w");
 
+    fprintf(myfile,"NowYear,NowMonth,NowPrecip,NowTemp,NowHeight,NowNumDeer,NowNumMushroom\n");
+    
     float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
 
     float temp = AVG_TEMP - AMP_TEMP * cos( ang );
@@ -73,9 +83,9 @@ int main () {
 	NowYear  = 2021;
 
 	// starting state (feel free to change this if you want):
-	NowNumDeer = 1;
+	NowNumDeer = 5;
 	NowHeight =  18.5f;
-
+	NowNumMushroom = 1;
     
     omp_set_num_threads( 4 );   // same as # of sections
     #pragma omp parallel sections
@@ -99,7 +109,9 @@ int main () {
         {
             MagicMushroom( ); // your own
         }
-    }}
+    }
+    fclose(myfile);
+    return 0;}
 
 void Deer() {
     while( NowYear < 2027 )
@@ -132,39 +144,6 @@ void Deer() {
         
     }}
 
-void Watcher() {
-    while( NowYear < 2027 )
-    {
-        
-        #pragma omp barrier
-        
-
-        // DoneAssigning barrier:
-        #pragma omp barrier
-        
-
-        printf("  %d   %d   %6.4lf   %6.4lf   %6.4lf   %d \n", NowYear, NowMonth, NowPrecip, (5.0f / 9.0f) * (NowTemp - 32.0f), NowHeight * 2.54f, NowNumDeer);
-        ++NowMonth;
-        if (NowMonth == 12)
-        {
-            ++NowYear;
-            NowMonth = 0;
-        }
-
-        float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
-
-        float temp = AVG_TEMP - AMP_TEMP * cos( ang );
-        NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
-
-        float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
-        NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
-        if( NowPrecip < 0. )
-            NowPrecip = 0.;
-
-        // DonePrinting barrier:
-        #pragma omp barrier
-        
-    }}
 
 void Grain() 
 {
@@ -178,8 +157,8 @@ void Grain()
         nextHeight += tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH;
         
        
-   		int deers_eating_mushroom = min((float)NowNumMushroom / DEER_EATS_MUSHROOM_PER_MONTH, NowNumDeer);
-   		int deers_eating_grain = NowNumDeer - deers_eating_mushroom;
+   		int deers_eating_mushroom = min((float)NowNumMushroom / DEER_EATS_MUSHROOM_PER_MONTH, (float)NowNumDeer);
+   		int deers_eating_grain = NowNumDeer - deers_eating_mushroom * DEER_EATS_MUSHROOM_PER_MONTH;
 
    		nextHeight -= (float)deers_eating_grain * ONE_DEER_EATS_PER_MONTH;
                                  
@@ -209,11 +188,32 @@ void MagicMushroom()
         
         float nextNumMushroom = NowNumMushroom;
 
-        if (NowTemp <= MAX_TEMP_MUSHROOM && NowTemp >= MAX_TEMP_MUSHROOM && NowPrecip >= MIN_PRECIPITATION)
+        if (NowTemp <= MAX_TEMP_MUSHROOM && NowTemp >= MIN_TEMP_MUSHROOM && NowPrecip >= MIN_PRECIP_MUSHROOM) 
+        {
+        	nextNumMushroom += MAGIC_GROWS_PER_MONTH;
+        }
+        else if (NowPrecip >= MIN_PRECIP_MUSHROOM && NowTemp > MAX_TEMP_MUSHROOM)
+        {
+        	nextNumMushroom += MAGIC_GROWS_PER_MONTH - 3;
+        }
+        else if (NowPrecip >= MIN_PRECIP_MUSHROOM && NowTemp < MIN_TEMP_MUSHROOM) 
+        {
+        	nextNumMushroom += MAGIC_GROWS_PER_MONTH - 5;
+        }
+        else if (NowPrecip < MIN_PRECIP_MUSHROOM && NowTemp > MAX_TEMP_MUSHROOM) {
+        	nextNumMushroom -= 10;
+        }
+
+        int deers_eating_mushroom = min((float)NowNumMushroom / DEER_EATS_MUSHROOM_PER_MONTH, (float)NowNumDeer);
+        nextNumMushroom -= deers_eating_mushroom * DEER_EATS_MUSHROOM_PER_MONTH;
+
+        if (nextNumMushroom < 0)
+        	nextNumMushroom = 0;
 
         // DoneComputing barrier:
         #pragma omp barrier
 
+      	NowNumMushroom = nextNumMushroom;
 
         // DoneAssigning barrier:
         #pragma omp barrier
@@ -223,3 +223,40 @@ void MagicMushroom()
     }
 
 }
+
+void Watcher() {
+    while( NowYear < 2027 )
+    {
+        
+        #pragma omp barrier
+        
+        // DoneAssigning barrier:
+        #pragma omp barrier
+        
+
+        //fprintf(myfile,"  %d   %d   %6.4lf   %6.4lf   %6.4lf   %d   %d \n", NowYear, NowMonth, NowPrecip, (5.0f / 9.0f) * (NowTemp - 32.0f), NowHeight * 2.54f, NowNumDeer, NowNumMushroom);
+        fprintf(myfile,"%d,%d,%6.4lf,%6.4lf,%6.4lf,%d,%d\n", NowYear, NowMonth, NowPrecip, (5.0f / 9.0f) * (NowTemp - 32.0f), NowHeight * 2.54f, NowNumDeer, NowNumMushroom);
+        
+       // fprintf(myfile,"%d,%d,%6.4lf,%6.4lf,%6.4lf,%d,%d\n", NowYear, NowMonth, NowPrecip, NowTemp, NowHeight , NowNumDeer, NowNumMushroom);
+
+        ++NowMonth;
+        if (NowMonth == 12)
+        {
+            ++NowYear;
+            NowMonth = 0;
+        }
+
+        float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
+
+        float temp = AVG_TEMP - AMP_TEMP * cos( ang );
+        NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
+
+        float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
+        NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
+        if( NowPrecip < 0. )
+            NowPrecip = 0.;
+
+        // DonePrinting barrier:
+        #pragma omp barrier
+        
+    }}
