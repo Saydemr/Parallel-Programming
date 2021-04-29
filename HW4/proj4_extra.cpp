416d770199cf57d20978c37fb67edd76337a7f88
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2021-04-30 00:23:33 +0300

    Parallelizing the SIMD code

--- HW4/proj4_extra.cpp
+++ HW4/proj4_extra.cpp
@@ -16,6 +16,13 @@ int NUMTRIES = 100;
 #define SIZE             1024
 #endif
 
+#define NUM_ELEMENTS_PER_CORE SIZE / NUMT
+
+float A[SIZE];
+float B[SIZE];
+float C[SIZE];
+
+
 
 void Mul( float *a, float *b,   float *c,   int len );
 float MulSum( float *a, float *b, int len );
@@ -30,13 +37,6 @@ int main(int argc, char const *argv[])
     	printf("No OpenMP support!\n" );
     return 1;
     #endif
-
-    if (argc >= 2)
-        SIZE = atoi(argv[1]);
-
-	float* A = new float[SIZE];
-	float* B = new float[SIZE];
-	float* C = new float[SIZE];
 
 	std::random_device rd;
 	std::mt19937 rng(rd());
@@ -58,40 +58,51 @@ int main(int argc, char const *argv[])
     double Mul_time = 9.9;
     double MulSum_time = 9.9;
 
-    
-		for (int i = 0; i < NUMTRIES; ++i)
+    omp_set_num_threads( NUMT );
+
+	for (int i = 0; i < NUMTRIES; ++i)
+	{
+		time0 = omp_get_wtime();
+		#pragma omp parallel
 		{
-			time0 = omp_get_wtime();
-			SimdMulSum(A,B,SIZE);
-			time1 = omp_get_wtime();
-
-			if((time1-time0) < SimdMulSum_time)
-				SimdMulSum_time = time1 - time0;
-
-
-			time0 = omp_get_wtime();
-			MulSum(A,B,SIZE);
-			time1 = omp_get_wtime();
-
-			if((time1-time0) < MulSum_time)
-				MulSum_time = time1 - time0;
-				
-
-			time0 = omp_get_wtime();
-			SimdMul(A,B,C,SIZE);
-			time1 = omp_get_wtime();
-
-			if((time1-time0) < SimdMul_time)
-				SimdMul_time = time1 - time0;
-
-
-			time0 = omp_get_wtime();
-			Mul(A,B,C,SIZE);
-			time1 = omp_get_wtime();
-
-			if((time1-time0) < Mul_time)
-				Mul_time = time1 - time0;
+			int first = omp_get_thread_num() * NUM_ELEMENTS_PER_CORE;
+			SimdMulSum(&A[first],&B[first], NUM_ELEMENTS_PER_CORE);	
 		}
+		
+		time1 = omp_get_wtime();
+
+		if((time1-time0) < SimdMulSum_time)
+			SimdMulSum_time = time1 - time0;
+
+
+		time0 = omp_get_wtime();
+		MulSum(A,B,SIZE);
+		time1 = omp_get_wtime();
+
+		if((time1-time0) < MulSum_time)
+			MulSum_time = time1 - time0;
+			
+
+		time0 = omp_get_wtime();
+		#pragma omp parallel
+		{
+			int first = omp_get_thread_num() * NUM_ELEMENTS_PER_CORE;
+			SimdMul(&A[first], &B[first], &C[first], NUM_ELEMENTS_PER_CORE);	
+		}
+		
+		time1 = omp_get_wtime();
+
+		if((time1-time0) < SimdMul_time)
+			SimdMul_time = time1 - time0;
+
+
+		time0 = omp_get_wtime();
+		Mul(A,B,C,SIZE);
+		time1 = omp_get_wtime();
+
+		if((time1-time0) < Mul_time)
+			Mul_time = time1 - time0;
+	}
 
 	// calculate the performance
 	double p1 = (double)SIZE/ SimdMulSum_time / 1000000.0;
@@ -99,18 +110,9 @@ int main(int argc, char const *argv[])
 	double p3 = (double)SIZE/ SimdMul_time / 1000000.0;
 	double p4 = (double)SIZE/ Mul_time / 1000000.0;
 
-
-	// calculate the speedup
-	double speedup_mulsum = (double)MulSum_time/(double)SimdMulSum_time;
-	double speedup_mul = (double)Mul_time/(double)SimdMul_time;
-
 	// commas for .csv file, use "proj4.sh > out.csv" in the terminal
-	printf("%10.3lf,%10.3lf,%10.3lf,%10.3lf,,%10.3lf,%10.3lf\n",p1, p2, p3, p4, speedup_mulsum, speedup_mul);
+	printf("%10.3lf,%10.3lf,%10.3lf,%10.3lf,\n",p1, p2, p3, p4);
 
-	delete A;
-	delete B;
-	delete C;
-	
 	return 0;
 }
 
@@ -118,7 +120,7 @@ int main(int argc, char const *argv[])
 void
 Mul( float *a, float *b,   float *c,   int len )
 {
-
+	#pragma omp parallel for default(none), shared(a,b,c,len)
 	for( int i = 0; i < len; ++i )
 	{
 		c[i] = a[i] * b[i];
@@ -131,6 +133,7 @@ MulSum( float *a, float *b, int len )
 {
 	float sum = 0.0f;
 
+	#pragma omp parallel for default(none), shared(a,b,len), reduction(+:sum)
 	for( int i = 0; i < len; ++i )
 	{
 		sum += a[i] * b[i];
--- HW4/proj4_extra.sh
+++ HW4/proj4_extra.sh
@@ -7,7 +7,7 @@ echo "NUMT, Size, SimdMulSum,  MulSum,    SimdMul,      Mul,, Speedup MulSum, Sp
 for t in 1 1 2 2 3 3 4 4 
 do
 
-    for s in 1024 10240 16000 32000 64000 128000 256000 512000 1000000 2000000 8000000
+    for s in 1024 2048 4096 8192 16384 32768 64000 128000 256000 512000 1024000 2048000
     do
     	echo -n $t","$s","
     	g++ -DNUMT=$t -DSIZE=$t proj4_extra.cpp -o proj4_ext -std=c++11 -lm -fopenmp
