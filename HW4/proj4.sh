0f9d362816ff95243234becb1d499ff2957f1de5
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2021-05-06 02:47:45 +0300

    sh

--- HW4/proj4.sh
+++ HW4/proj4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Size, SimdMulSum,  MulSum,    SimdMul,      Mul,, Speedup MulSum, Speedup Mul"
-for t in 1024 4096 10240 32768 40000 48000 64000 128000 256000 512000 1024000 2048000
+for t in 1024 4096 10240 32768 48000 64000 128000 250000 500000 1000000 2000000
 do
 	echo -n $t","
 	g++ -DSIZE=$t project4.cpp -o proj4 -std=c++11 -lm -fopenmp
